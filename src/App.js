c3c703c1ffa98e6fbdb970b12d951863cc5bd2f2
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-03-17 23:55:49 +0530

    feat: completed basic features of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,91 @@
-import './App.css';
+import "./styles.css";
+import { Routes, Route, NavLink } from "react-router-dom";
 
-function App() {
+import { Home } from "./Pages/Home";
+import { Explore } from "./Pages/Explore";
+import { Bookmark } from "./Pages/Bookmark";
+import { People } from "./Pages/People";
+import { PeopleProfile } from "./Pages/PeopleProfile";
+import { Profile } from "./Pages/Profile";
+import { Signup } from "./Pages/Signup";
+import { Login } from "./Pages/Login";
+import { AuthContext } from "./index";
+
+import { RequiresAuth } from "./Components/RequiresAuth";
+import { useContext } from "react";
+
+export default function App() {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   return (
     <div className="App">
-      <h1>React</h1>
+      {isLoggedIn && (
+        <nav className="nav">
+          <NavLink className="NavLink" to="/">
+            Home
+          </NavLink>
+          <NavLink className="NavLink" to="/explore">
+            Explore
+          </NavLink>
+          <NavLink className="NavLink" to="/bookmark">
+            Bookmark
+          </NavLink>
+          <NavLink className="NavLink" to="/profile">
+            Profile
+          </NavLink>
+          <NavLink className="NavLink" onClick={() => setIsLoggedIn(false)}>
+            Logout
+          </NavLink>
+        </nav>
+      )}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequiresAuth>
+              <Home />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/explore"
+          element={
+            <RequiresAuth>
+              <Explore />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/bookmark"
+          element={
+            <RequiresAuth>
+              <Bookmark />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <RequiresAuth>
+              <Profile />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/peopleProfile"
+          element={
+            <RequiresAuth>
+              <PeopleProfile />
+            </RequiresAuth>
+          }
+        />
+        <Route path="/signUp" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {isLoggedIn && (
+        <div className="route">
+          <People />
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
+
+import { PostContext, PostProvider } from "./Contexts/PostContext";
+import { BookmarkContext, BookmarkProvider } from "./Contexts/BookmarkContext";
+import { UserContext, UserProvider } from "./Contexts/UserContext";
+import { AuthContext, AuthProvider } from "./Contexts/AuthContext";
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
+export { PostContext };
+export { BookmarkContext };
+export { UserContext };
+export { AuthContext };
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Router>
+      <AuthProvider>
+        <BookmarkProvider>
+          <PostProvider>
+            <UserProvider>
+              <App />
+            </UserProvider>
+          </PostProvider>
+        </BookmarkProvider>
+      </AuthProvider>
+    </Router>
+  </StrictMode>
 );
 
